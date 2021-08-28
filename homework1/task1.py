b542918c81160c18eafef96f77ccbc2907ea343a
TatianaNik <89660210+TatianaNik@users.noreply.github.com>
2021-08-28 17:49:32 +0300

    Update task1.py

--- homework1/task1.py
+++ homework1/task1.py
@@ -1,2 +1,11 @@
-def solution():
-    return 1
+def check_power_of_2(a: int) -> bool:
+    b = a ** 0.5
+    if (b - int(b)) == 0:
+        print ("True")
+        return True
+    else:
+        print ("False")
+        return False
+
+
+ 

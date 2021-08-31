a9153d2d0ded56db274abd86b0f8ce5927db0abd
TatianaNik <89660210+TatianaNik@users.noreply.github.com>
2021-08-31 08:41:11 +0300

    Update test_task1.py

--- tests/homework0/test_task1.py
+++ tests/homework0/test_task1.py
@@ -1,5 +1,11 @@
-from homework0.task1 import some_function
+import pytest
 
 
-def test_some_function():
-    assert some_function()
+from homework1.task1 import check_power_of_2()
+
+
+def test_positive_case():
+    assert check_power_of_2(1024)
+    
+def test_negative_case():
+    assert not check_power_of_2(14)

a6210f21d7354ad16d126e6af1bd00f1b3750741
Tatiana Nikolaeva <tatiananikolaeva@yandex.ru>
2021-10-20 08:29:53 +0300

    Task1 was modified

--- tests/homework9/test_task1.py
+++ tests/homework9/test_task1.py
@@ -1,13 +1,21 @@
+import pytest_mock
+
 from homework9.task1 import merge_sorted_files
 
 
-file_list1 = ['file1.txt', 'file2.txt', 'file3.txt']
+def test_merge_sorted_files_mock(mocker):
+    m = mocker.patch('file1.txt', return_value='1, 3, 5')
+    n = mocker.patch('file2.txt', return_value='2, 4, 6')
+    k = mocker.patch('file3.txt', return_value='7, 8, 9')
+    assert(list(merge_sorted_files([m, n, k]) == [1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
-def test_merge_sorted_files_3_files():
-    assert(list(merge_sorted_files(file_list1)) == [1,2,3,4,5,6,7,8,9])
-
-
-file_list2 = ['file3.txt', 'file4.txt']
-
-def test_merge_sorted_files_2_files_dif_length():
-    assert(list(merge_sorted_files(file_list2)) == [5,7,8,9,10,11,12])
+# file_list1 = ['file1.txt', 'file2.txt', 'file3.txt']
+#
+# def test_merge_sorted_files_3_files():
+#     assert(list(merge_sorted_files(file_list1)) == [1,2,3,4,5,6,7,8,9])
+#
+#
+# file_list2 = ['file3.txt', 'file4.txt']
+#
+# def test_merge_sorted_files_2_files_dif_length():
+#     assert(list(merge_sorted_files(file_list2)) == [5,7,8,9,10,11,12])

a736d00d67719f57d14f8f37eee2bbf49e12d5a3
Tatiana Nikolaeva <tatiananikolaeva@yandex.ru>
2021-10-25 16:13:50 +0300

    task 1 hw 10 - 3d version, some corrections were made

--- homework10/hw10_task1.py
+++ homework10/hw10_task1.py
@@ -18,6 +18,7 @@ def get_company_list(p_url):  # processing one page out of 12
     html_insider = BeautifulSoup(response.text, features="html.parser")
     h = html_insider.body
     body = h.find("tbody", class_="table__tbody")
+    companies = []
     for row in body.findAll("tr"):
         company = {}
         for cell in row.findAll('td'):
@@ -45,27 +46,29 @@ def get_company_list(p_url):  # processing one page out of 12
         if row.findAll('span', class_="colorGreen"):
             for cell in row.findAll('span', class_="colorGreen")[-1]:
                 company['1_year'] = float(cell.text.replace('%',''))
-    return company
+        companies.append(company)
+    res = [x for x in a for a in companies]
+    print(res)
+    return res
 
 
 with ThreadPoolExecutor(max_workers=12) as pool:
-    companies = list(pool.map(get_company_list, pages_urls))
-    # print(companies)
+    comps = list(pool.map(get_company_list, pages_urls))
+    print(len(comps))
 
-
-sorted_by_price = sorted(companies, key=lambda k: k['value'])
-ten_most_expensive = sorted_by_price[-10:-1]  # ten companies with most expensive stocks???? is that right?
-sorted_by_P_E_Ratio = sorted(companies, key=lambda k: k['P/E Ratio'])
-ten_lowest_P_E_Ratio = sorted_by_P_E_Ratio[0:10]  # ten companies with lowest P/E Ratio
-sorted_by_growth = sorted(companies, key=lambda k: k['1_year'])
-ten_biggest_growth = sorted_by_growth[-10:-1]    # ten companies with biggest 1 year growth
-sorted_by_pot_profit = sorted(companies, key=lambda k: k['potential_profit_%'])
-ten_biggest_pt_profit = sorted_by_pot_profit[-10:-1]   # ten companies with biggest potential profit
-
-curr = requests.get('https://www.cbr-xml-daily.ru/daily_json.js') #  current usd values
-curr_usd = curr.json()['Valute']['USD']['Value']
-
-for one in ten_most_expensive:
-    one["value"] = one["value"] * curr_usd
-
-print(ten_most_expensive)
+# sorted_by_price = sorted(comps, key=lambda k: k['value'], reverse=True)
+# ten_most_expensive = sorted_by_price[10]  # ten companies with most expensive stocks???? is that right?
+# sorted_by_P_E_Ratio = sorted(comps, key=lambda k: k['P/E Ratio'])
+# ten_lowest_P_E_Ratio = sorted_by_P_E_Ratio[0:10]  # ten companies with lowest P/E Ratio
+# sorted_by_growth = sorted(comps, key=lambda k: k['1_year'], reverse=true)
+# ten_biggest_growth = sorted_by_growth[10]    # ten companies with biggest 1 year growth
+# sorted_by_pot_profit = sorted(comps, key=lambda k: k['potential_profit_%'], reverse=True)
+# ten_biggest_pt_profit = sorted_by_pot_profit[10]   # ten companies with biggest potential profit
+#
+# curr = requests.get('https://www.cbr-xml-daily.ru/daily_json.js') #  current usd values
+# curr_usd = curr.json()['Valute']['USD']['Value']
+#
+# for one in ten_most_expensive:
+#     one["value"] = one["value"] * curr_usd
+#
+# print(ten_most_expensive)
